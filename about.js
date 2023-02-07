d1438e8bc329c056200949117f3dee86953b45ba
Jaime Villegas <jaimevillegas296@gmail.com>
2023-02-07 14:47:50 -0500

    Add about.js and work on modal dialog

---
+++ about.js
@@ -0,0 +1,17 @@
+const navMenu = document.getElementById('modal-menu');
+const open = document.getElementById('open-modal-menu');
+const close = document.getElementById('close-modal-menu');
+const menuAbout = document.getElementById('about-page');
+const menuItem = document.querySelectorAll('.menu-item');
+
+function mobileMenuDisplayNone() {
+    navMenu.style.display = 'none';
+}
+
+function mobileMenuDisplayBlock() {
+    navMenu.style.display = 'block';
+}
+
+open.addEventListener('click', (() => { mobileMenuDisplayBlock(); }));
+close.addEventListener('click', (() => { mobileMenuDisplayNone(); }));
+menuItem.addEventListener('click', (() => { mobileMenuDisplayNone(); }));
